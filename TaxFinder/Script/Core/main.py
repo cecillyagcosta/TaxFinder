c56a29719c9fc810a50c61fb9d45ba1a59fbda44
Cecillya  Costa <cecillyagcosta@gmail.com>
2022-09-03 01:51:59 -0300

    1.0.2

--- TaxFinder/Script/Core/main.py
+++ TaxFinder/Script/Core/main.py
@@ -3,22 +3,13 @@ from typing import final
 import xmltreatment as xmlt
 import pandas as pd
 
-def createCSV(_self_):
-    fields = ['Tipo de nota', 'Data', 'No de Nota', 'ICMS', 'IPI']
-    rows = [[_self_[0],_self_[2], _self_[1], _self_[3], _self_[4]]]
-    with open('info899.csv', 'w') as file:
-        write = csv.writer(file)
-        write.writerow(fields)
-        write.writerows(rows)
-        file.close()
 
 info = xmlt.extractXMLInfo('DANFE157411.xml')
 print(info)
 for each in info:
     print(each)
-createCSV(info)
 df = pd.read_csv('info899.csv')
 df.index = df['Tipo de nota']
 df = df.drop('Tipo de nota', axis=1)
 print(df.head())
-#df.to_excel('exemplofinal.xlsx')
+df.to_excel('exemplofinal.xlsx')
